c604bd743e409a329c04229cc16bc32b1cc32c78
AlvaroPlaza <57074350+AlvaroPlaza@users.noreply.github.com>
2022-04-04 13:54:54 +0200

    probando elementos por fila dinamicamente

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,7 +13,7 @@ VALUES (4, 'RESTAURANTE', TRUE, 'RESTAURANTE,USER', 'restaurante',
     '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
 -- TABLA RESTAURANTE
 INSERT INTO Restaurante (id, nombre, direccion, horario, descripcion, valoracion, propietario_id)
-VALUES (1,'Vips', 'Calle Falsa, 123', '24/7', 'Lorem ipsum', 4.0, 4);
+VALUES (1,'Vips', 'Calle Falsa, 123', '24/7', 'Te queremos en Vips', 4.0, 4);
 INSERT INTO Restaurante (id, nombre, direccion, horario, descripcion, valoracion, propietario_id)
 VALUES (2,'Fosters Hollywood', 'Calle Falsa, 123', '24/7', 'Lorem ipsum', 4.0, 4);
 INSERT INTO Restaurante (id, nombre, direccion, horario, descripcion, valoracion, propietario_id)
